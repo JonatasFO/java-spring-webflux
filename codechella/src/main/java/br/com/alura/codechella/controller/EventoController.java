70aae9f6ce406e45e6a43ab3d2239b98d6b2cbf7
Jonatas Ferreira <jonatasoliveira.ext@orbitaltech.com.br>
2026-01-25 02:31:47 -0300

    crud

--- codechella/src/main/java/br/com/alura/codechella/controller/EventoController.java
+++ codechella/src/main/java/br/com/alura/codechella/controller/EventoController.java
@@ -1,25 +1,43 @@
 package br.com.alura.codechella.controller;
 
-import br.com.alura.codechella.model.Evento;
-import br.com.alura.codechella.repository.EventoRepository;
+import br.com.alura.codechella.dto.EventoDto;
+import br.com.alura.codechella.service.EventoService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/eventos")
 @AllArgsConstructor
 public class EventoController {
 
-    private EventoRepository repository;
+    private EventoService service;
 
-    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Evento> obterTodos() {
-        return repository.findAll();
+    @GetMapping//(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<EventoDto> obterTodos() {
+        return service.obterTodos();
     }
 
+    @GetMapping("/{id}")
+    public Mono<EventoDto> obterPorId(@PathVariable Long id) {
+        return service.obterPorId(id);
+    }
+
+    @PostMapping
+    public Mono<EventoDto> cadastrar(@RequestBody EventoDto eventoDto) {
+        return service.cadastar(eventoDto);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<EventoDto> atualizar(@PathVariable Long id, @RequestBody EventoDto eventoDto) {
+        return service.atualizar(id, eventoDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> excluir(@PathVariable Long id) {
+        return service.excluir(id);
+    }
 
 }
---
+++ codechella/src/main/java/br/com/alura/codechella/service/EventoService.java
@@ -0,0 +1,52 @@
+package br.com.alura.codechella.service;
+
+import br.com.alura.codechella.dto.EventoDto;
+import br.com.alura.codechella.model.Evento;
+import br.com.alura.codechella.repository.EventoRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+@AllArgsConstructor
+public class EventoService {
+    private EventoRepository repository;
+
+    public Flux<EventoDto> obterTodos() {
+        return repository.findAll().map(EventoDto::toDto);
+    }
+
+    public Mono<EventoDto> obterPorId(Long id) {
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
+                .map(EventoDto::toDto);
+    }
+
+
+    public Mono<EventoDto> cadastar(EventoDto eventoDto) {
+        return repository.save(eventoDto.toModel()).map(EventoDto::toDto);
+    }
+
+    public Mono<EventoDto> atualizar(Long id, EventoDto eventoDto) {
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Id do evento não encontrado.")))
+                .flatMap(evento -> atualizarEvento(evento, eventoDto))
+                .map(EventoDto::toDto);
+    }
+
+    private Mono<Evento> atualizarEvento(Evento evento, EventoDto eventoDto) {
+        evento.setTipo(eventoDto.tipo());
+        evento.setNome(eventoDto.nome());
+        evento.setDescricao(eventoDto.descricao());
+        evento.setData(eventoDto.data());
+
+        return repository.save(evento);
+    }
+
+    public Mono<Void> excluir(Long id) {
+        return repository.findById(id).flatMap(repository::delete);
+    }
+}
